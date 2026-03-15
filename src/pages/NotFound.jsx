e29581acd4093c0c505e502af3b8e00c8b67d54f
KylanG <KylanG98@outlook.com>
2026-03-15 20:32:10 +0100

    change text size for 404 header

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -6,7 +6,7 @@ export default function NotFound({ darkMode, setDarkMode }) {
     <div className={`h-screen flex flex-col transition-colors duration-300 ${darkMode ? 'bg-black text-white' : 'bg-white text-black'}`}>
       <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
       <main className="flex-1 flex flex-col items-center justify-center text-center px-6">
-        <h1 className="font-primary uppercase text-8xl mb-4">Page not found</h1>
+        <h1 className="font-primary uppercase text-5xl mb-4">Page not found</h1>
         <p className="font-secondary text-lg mb-8">The page you are looking for doesn’t 
         exist or has been moved.</p>
         <a href="/" className={`flex justify-center gap-2 p-4 rounded-full border font-secondary leading-none ${darkMode ? 'border-white bg-white text-black' : 'border-black bg-black text-white'}`}>
