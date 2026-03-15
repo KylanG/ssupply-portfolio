d055db5d48352b7615722c36ee4649133508876c
KylanG <KylanG98@outlook.com>
2026-03-15 20:31:05 +0100

    change text for 404 and add button back to home

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -6,8 +6,12 @@ export default function NotFound({ darkMode, setDarkMode }) {
     <div className={`h-screen flex flex-col transition-colors duration-300 ${darkMode ? 'bg-black text-white' : 'bg-white text-black'}`}>
       <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
       <main className="flex-1 flex flex-col items-center justify-center text-center px-6">
-        <h1 className="font-primary uppercase text-8xl mb-4">404</h1>
-        <p className="font-secondary text-lg mb-8">Oops! This page doesn't exist.</p>
+        <h1 className="font-primary uppercase text-8xl mb-4">Page not found</h1>
+        <p className="font-secondary text-lg mb-8">The page you are looking for doesn’t 
+        exist or has been moved.</p>
+        <a href="/" className={`flex justify-center gap-2 p-4 rounded-full border font-secondary leading-none ${darkMode ? 'border-white bg-white text-black' : 'border-black bg-black text-white'}`}>
+            <span>Back to home</span>👋🏻
+          </a>
       </main>
       <Footer darkMode={darkMode} />
     </div>
