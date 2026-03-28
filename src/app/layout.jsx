c47622290db9e7f16f00b2585a7a7c5132aa13e9
KylanG <KylanG98@outlook.com>
2026-03-28 02:33:01 +0100

    remove font preloads that hurt mobile Lighthouse score

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -14,22 +14,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <link
-          rel="preload"
-          href="/fonts/ApfelGrotezk-Regular.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          href="/fonts/6849a000350c88ee79310a0c_Bueno-Bold.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-      </head>
       <body>
         <ClientProviders>
           {children}
